853210c4f1372121c7d49b4a23fa6c7386afa082
PIERO90 <134454830+PIERO90@users.noreply.github.com>
2024-02-29 17:11:47 -0300

    first commit

---
+++ poke-contxt/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink className={({ isActive }) => (isActive ? "active-link" : undefined)} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className={({ isActive }) => (isActive ? "active-link" : undefined)} to="/pokemon">
+            Pokemon
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ poke-contxt/src/views/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        {" "}
+        <h2>Bienvenidos Entrenadores</h2>
+        <img src="https://paranoiasgamersweb.com/wp-content/uploads/2024/01/Pokemon.webp" alt="Bienvenido a la Pokédex" className="welcome-image" />
+      </div>
+    </>
+  );
+};
+
+export default Home;
